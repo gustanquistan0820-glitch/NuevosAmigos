291c57748d9681fc93e0945fe11768c275543c7d
gustanquistan0820-glitch <gustanquistan0820@gmail.com>
2025-09-14 00:00:11 -0400

    Add files via upload

---
+++ Amigo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package amigo;
+
+/**
+ *
+ * @author KUAA
+ */
+public class Amigo {
+    private String nombre;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
